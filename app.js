1cbdce208e43ce2290f9ef941c584bbf5b3728bf
AshisPatel <ashis.n.patel@gmail.com>
2021-09-05 12:52:31 -0500

    Add table for roles, add prompt to get new roles, partially add prompt to get department names for new role creation

--- app.js
+++ app.js
@@ -1,45 +1,152 @@
-const Database = require('./lib/Database');
+const Department = require('./lib/Department');
+const Roles = require('./lib/Roles');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
-const db = new Database; 
 
-const start = function() {
+const start = function () {
     return inquirer
         .prompt([
             {
-                type:'list',
+                type: 'list',
                 name: 'action',
                 message: 'What would you like to do?',
-                choices: ['View all departments', 'Add a department', 'Quit']
-            },
-            {
-                type: 'input',
-                name: 'departmentName',
-                message: 'Enter the new department name: ',
-                when: ({ action }) => {
-                    if (action === 'Add a department') {
-                        return true; 
-                    } else {
-                        return false; 
-                    }
-                }
+                choices: ['View all departments', 'View all roles', 'Modify departments', 'Modify roles', 'Quit']
             }
-         ])
-        .then(answer => {
-            const action = answer.action; 
+        ])
+        .then(data => {
+            const action = data.action;
             if (action === 'View all departments') {
-                db.getDepartments(); 
+                const department = new Department;
+                return department.getDepartments();
+            }
+            if (action === 'View all roles') {
+                const role = new Roles;
+                return role.getRoles();
             }
 
-            if (answer.departmentName) {
-                db.addDepartment(answer.departmentName);
+            if (action === 'Modify departments') {
+                return departmentPrompts();
             }
-            
+
+            if (action === 'Modify roles') {
+                return rolePrompts();
+            }
+
             if (action === 'Quit') {
                 return;
             }
-            return start();
+
+        })
+};
+
+const departmentPrompts = function () {
+    console.log('Taking you to the department options...');
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: ['Add a new department', 'Go back']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the new department name:',
+            when: ({ action }) => {
+                if (action === 'Add a new department') {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ])
+        .then(data => {
+            if (data.action === 'Go back') {
+                return start();
+            }
+
+            const department = new Department;
+            if (data.name) {
+                return departments.addDepartment(data.name);
+            }
+
         })
 }
 
-start();
+const rolePrompts = function () {
+    console.log('Taking you to the role prompts...')
+    const department = new Department;
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: ['Add a new role', 'Go back']
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter new role title: ',
+            when: ({action}) => {
+                if(action === 'Add a new role') {
+                    return true; 
+                } else {
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter new role salary: ',
+            when: ({action}) => {
+                if(action === 'Add a new role') {
+                    return true; 
+                } else {
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'department',
+            message: 'Choose the department of the new role: ',
+            choices: [department.getDepartmentNames()],
+            when: ({action}) => {
+                if(action === 'Add a new role') {
+                    return true; 
+                } else {
+                    return false; 
+                }
+            }
+        }
+    ])
+        .then(data => {
+            if (data.action === 'Go back') {
+                return start();
+            }
+
+            const roles = new Roles;
+            if (data.action === 'Add a new role') {
+                return roles.addDepartment(data.title, data.salary, data.department)
+            }
+        })
+}
+
+//start();
+
+const test = new Department;
+
+function print(result) {
+    console.log(result); 
+}
+
+print(test.getDepartmentNames());
+
+
+
+
+
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,12 @@ VALUES
 ('Engineering'),
 ('Finance'),
 ('Legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Salesman', 80000, 1),
+('Sales Manager', 120000, 1),
+('Software Engineer', 100000, 2),
+('Accountant', 90000, 3),
+('Finance Manager', 150000, 3),
+('Lawyer', 190000, 4);
