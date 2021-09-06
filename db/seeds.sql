f4796c8db3ec385a30ae2ebf881ad15a88df6124
AP <ashis.n.patel@gmail.com>
2021-09-05 20:40:05 -0500

    Add employee schema/seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,12 @@ VALUES
 ('Accountant', 90000, 3),
 ('Finance Manager', 150000, 3),
 ('Lawyer', 190000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Alpha', 'Chad', 2, 2),
+('Sabrina', 'The Witch', 6, NULL),
+('Himbo', 'Vibes', 1, 1),
+('Frazzled', 'Koala', 4, 5),
+('CASH', 'MONEY', 5, NULL),
+('Hacker', 'Person', 3, 2);
